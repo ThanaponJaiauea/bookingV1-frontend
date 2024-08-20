b1b5fcf19ebb4f694b1697f880be2009ef154a88
ThanaponJaiauea <ham@yojoies.com>
2024-08-20 10:37:18 +0700

    #

--- src/pages/roomPage.jsx
+++ src/pages/roomPage.jsx
@@ -60,8 +60,8 @@ export default function RoomPage() {
   const [child, setChild] = useState(0)
   const [infant, setInfant] = useState(0)
   const [quantity, setQuantity] = useState()
-
   const [scores, setScores] = useState(5)
+  const [openGallery, setOpenGallery] = useState(false)
 
   const incrementGuests = (type) => {
     if (type === "adult" && adult < 4 - child - infant) setAdult(adult + 1)
@@ -122,7 +122,7 @@ export default function RoomPage() {
           </div>
         </div>
 
-        {/*gallery  */}
+        {/* gallery  */}
         <div className="flex gap-2 ">
           <div className="w-[50%] h-full">
             <img
@@ -145,7 +145,9 @@ export default function RoomPage() {
               </div>
             ))}
 
-            <button className="absolute flex items-center gap-2 bottom-8 right-8 border-[1px] border-black bg-white rounded-lg px-4 py-2 hover:bg-gray-100">
+            <button
+              onClick={() => setOpenGallery(!openGallery)}
+              className="absolute flex items-center gap-2 bottom-8 right-8 border-[1px] border-black bg-white rounded-lg px-4 py-2 hover:bg-gray-100">
               <BsGrid3X3Gap />
               แสดงรูปทั้งหมด
             </button>
@@ -440,6 +442,73 @@ export default function RoomPage() {
           </div>
         </div>
       </div>
+
+      {openGallery && (
+        <div
+          id="default-modal"
+          tabindex="-1"
+          className="w-full h-full fixed top-0 right-0 left-0 z-50 flex justify-center items-center md:inset-0 h-[calc(100%-1rem)] bg-black bg-opacity-30 backdrop-blur-sm">
+          <div className="relative p-4 w-full max-w-2xl max-h-full">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  Terms of Service
+                </h3>
+                <button
+                  onClick={() => setOpenGallery(false)}
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="default-modal">
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14">
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              <div className="p-4 md:p-5 space-y-4">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  With less than a month to go before the European Union enacts
+                  new consumer privacy laws for its citizens, companies around
+                  the world are updating their terms of service agreements to
+                  comply.
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  The European Union’s General Data Protection Regulation
+                  (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
+                  common set of data rights in the European Union. It requires
+                  organizations to notify users as soon as possible of high-risk
+                  data breaches that could personally affect them.
+                </p>
+              </div>
+              <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button
+                  data-modal-hide="default-modal"
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  I accept
+                </button>
+                <button
+                  data-modal-hide="default-modal"
+                  type="button"
+                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                  Decline
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
